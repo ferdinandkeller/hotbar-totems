320d412296dfabd30a7908762e5d13af2ff5f53b
Ferdinand Keller <ferdinand.keller@proton.me>
2026-08-14 20:19:29 +0200

    v1.0.1+26.2

--- src/main/java/dev/ferdinandkeller/hotbartotems/mixin/HotbarTotemsMixin.java
+++ src/main/java/dev/ferdinandkeller/hotbartotems/mixin/HotbarTotemsMixin.java
@@ -1,6 +1,6 @@
 package dev.ferdinandkeller.hotbartotems.mixin;
 
-import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.triggers.CriteriaTriggers;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
